42e91a3a8ddc10cfed475b721c08f9dab5e194e3
秋秋 <973146586@qq.com>
2023-07-19 10:12:19 +0800

    modified page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-
+import { useEffect } from "react";
 import { Header } from "@/components/header";
 import { Connection } from "@/components/connection";
+import { usePostNote } from "@crossbell/connect-kit";
 
 export default function IndexPage() {
+  const postNote = usePostNote();
+
+  useEffect(() => {}, []);
   return (
     <div>
       <Header />
       <Connection />
+      <button
+        onClick={() => {
+          postNote.mutate({
+            metadata: {
+              content: "Hello Word!",
+              sources: ["Crossbell Dev"],
+              external_urls: ["https://crossbell.io"],
+              tags: ["post"],
+            },
+          });
+        }}
+      >
+        Post Note
+      </button>
     </div>
   );
 }
